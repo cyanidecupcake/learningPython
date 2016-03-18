3f01bf4763a0bc75e2e3ab0c1956a7be3f81a641
Cyanide Cupcake <jlweichler@gmail.com>
2016-03-18 23:00:19 +1300

    Improvements
    Added loops, quit dialogue, and waits

--- dice.py
+++ dice.py
@@ -1,24 +1,41 @@
 import random
 import time
 
-player = random.randint(1,6)
-ai = random.randint(1,6)
 
-print("Press return to roll your die")
+def dice() :   ##Function
 
-roll = raw_input()
-print("You rolled " + str(player) )
-time.sleep(5)
-print("The computer rolled " + str(ai) )
-time.sleep(2)
+	player = random.randint(1,6)
+	ai = random.randint(1,6)
+
+	print("You rolled " + str(player) )
+	time.sleep(5)
+	print("The computer rolled " + str(ai) )
+	time.sleep(3)
 
 
-if player > ai : 
-	print("You Win")
-elif player == ai :
-	print("Much like in life, no one won this round")
-else :
-	print("You Lose")
+	if player > ai : 
+		print("You Win")
+	elif player == ai :
+		print("Much like in life, no one won this round")
+	else :
+		print("You Lose")
+
 	
+	time.sleep(4)
+	
+	print("")
+	print("Would you like to quit? Y/N") 
+	cont = raw_input()
+	if cont == "Y" :
+		exit()
+	else :
+		pass
+	
+	
+while True :
+	print(" ")
+	print("Press return to roll your die")
+	roll = raw_input()
+	dice()
 	
 	
